4f430611a5b542cd909e12ed4cf521e1c8c3b4e7
DrunkenCloud <drunkencloud99@gmail.com>
2025-06-22 20:50:12 +0530

    Finished product

--- 8_lab/obf.c
+++ 8_lab/obf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 unsigned char encoded_secret_key[] = {
     0xEE, 0xF2, 0xAA, 0xAE, 0xAC, 0xAF, 0xAB, 0xE9, 0xAA, 0xA2, 0xAF, 0xA4, 0xA8, 0xE8, 0xEA, 0xEB, 0xAE, 0xAC, 0xA5, 0xA7, 0xAD, 0xAE, 0xAF, 0x00 // Null terminator
@@ -7,7 +8,7 @@ unsigned char xor_key = 0xAA;
 
 void decode_string(unsigned char *encoded_str, unsigned char key) {
     int i = 0;
-    while (encoded_str[i] != 0) { 
+    while (encoded_str[i] != 0) {
         encoded_str[i] = encoded_str[i] ^ key;
         i++;
     }
@@ -17,13 +18,25 @@ int validate_key(char *input_key) {
     char decoded_key[sizeof(encoded_secret_key)];
     memcpy(decoded_key, encoded_secret_key, sizeof(encoded_secret_key));
 
-    decode_string((unsigned char *)decoded_key, xor_key);  
+    decode_string((unsigned char *)decoded_key, xor_key);
 
     if (strcmp(input_key, decoded_key) == 0) {
-        printf("Correct!");
+        printf("Correct!\n");
         return 1;
     } else {
-        printf("Wrong!");
+        printf("Wrong!\n");
         return 0;
     }
+}
+
+int main() {
+    char user_input[100];
+    printf("Enter the secret key: ");
+    fgets(user_input, sizeof(user_input), stdin);
+
+    // Remove trailing newline from fgets
+    user_input[strcspn(user_input, "\n")] = 0;
+
+    validate_key(user_input);
+    return 0;
 }
